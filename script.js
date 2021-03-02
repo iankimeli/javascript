9f37d2542b525646dbedf02008f0a65560aff10c
Ian <iankimeli@gmail.com>
2021-03-02 10:46:45 +0300

    akan names

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let outputTag = document.getElementById('output')
-
-
-function getName(event){
-    event.preventDefault()
-    let date = document.getElementById(date).value
-    let gender = document.getElementById('gender').value
-    let myBirthday = newDate(date).toDateString()
-    let day = myBirthday.split(' ')[0]
-    let message =`${day}`
-    message = hello day
-}
-
-outputTag.innerHTML = message
-
+function getName()
+        {
+            var female_names=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+            var male_names=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+            var date=new Date(document.getElementById("users_date").value); //get the date from the html element
+            var gender=document.getElementById("gender").value.toLowerCase(); //get the gender
+            var day=date.getDay();
+            //getDay() -- day of the week 0 for sunday upto 6 saturday
+            
+            if (gender==="male")
+            {document.getElementById("akan_name").innerHTML=male_names[day];}
+            else
+            {document.getElementById("akan_name").innerHTML=female_names[day];}
+            
+            return false;
+        }   
+        
+        function clearAll()
+        {
+            document.getElementById("akan_name").innerHTML="";
+        } 
